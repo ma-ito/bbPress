d387f7dd17eb73eda31bb3a855509b28be866ae3
ma-ito <ito.masahiro@cij.co.jp>
2013-10-04 10:11:46 +0900

    検索画面でのロール表示削除

--- templates/default/bbpress/loop-search-topic.php
+++ templates/default/bbpress/loop-search-topic.php
@@ -60,7 +60,7 @@
 
 		<?php do_action( 'bbp_theme_before_topic_author_details' ); ?>
 
-		<?php bbp_topic_author_link( array( 'sep' => '<br />', 'show_role' => true ) ); ?>
+		<?php bbp_topic_author_link( array( 'sep' => '<br />', 'show_role' => false ) ); ?>
 
 		<?php if ( bbp_is_user_keymaster() ) : ?>
 
